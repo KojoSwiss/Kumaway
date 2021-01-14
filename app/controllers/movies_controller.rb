96b1fe25cf45f9fc4ac043959677d0cc7ad4b163
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-13 19:08:59 -0500

    Edit, Show actions and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @movies = Movie.all.order("created_at DESC")
   end
 
   def new
@@ -10,11 +11,34 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render 'edit'
   end
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :description, :director)
+  end
+
+  def find_movie
+   @movie = Movie.find(params[:id])
   end
 end
